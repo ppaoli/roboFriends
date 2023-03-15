8ad30615fa1e002b093c4e744c8952400f78ceb3
Philippe Paoli <philippe.paoli11@gmail.com>
2023-03-15 17:16:09 +0000

    first react app

---
+++ src/Containers/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import CardList from "../Components/CardList";
+import Scroll from "../Components/Scroll";
+import SearchBox from "../Components/SearchBox";
+import ErrorBoundry from "../Components/ErrorBoundry";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value }); // very important to change State
+  };
+
+  render() {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    });
+    return !robots.length ? (
+      <h1>Loading</h1> // evaluating if there is no robot we're loading
+    ) : (
+      <div className="tc">
+        <h1> RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <ErrorBoundry>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundry>
+        </Scroll>
+      </div>
+    );
+  }
+}
+
+export default App;
