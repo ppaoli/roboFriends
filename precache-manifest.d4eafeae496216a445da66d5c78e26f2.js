5e5c9d3f81b55ea0ba48aa3c1ab9041da3120f85
Philippe Paoli <philippe.paoli11@gmail.com>
2023-03-15 17:33:23 +0000

    Updates

---
+++ precache-manifest.d4eafeae496216a445da66d5c78e26f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6dfd54640d399c9568c2",
+    "url": "/roboFriends/static/css/main.040ee5bc.chunk.css"
+  },
+  {
+    "revision": "6dfd54640d399c9568c2",
+    "url": "/roboFriends/static/js/main.6dfd5464.chunk.js"
+  },
+  {
+    "revision": "3f5a8f4c7bc0532f173e",
+    "url": "/roboFriends/static/js/1.3f5a8f4c.chunk.js"
+  },
+  {
+    "revision": "d42babd0923c43734bd4",
+    "url": "/roboFriends/static/css/2.b130baee.chunk.css"
+  },
+  {
+    "revision": "d42babd0923c43734bd4",
+    "url": "/roboFriends/static/js/2.d42babd0.chunk.js"
+  },
+  {
+    "revision": "a4b7c733913c97416962",
+    "url": "/roboFriends/static/js/runtime~main.a4b7c733.js"
+  },
+  {
+    "revision": "b320f92cb9160607705a4fe68a963d23",
+    "url": "/roboFriends/index.html"
+  }
+];
